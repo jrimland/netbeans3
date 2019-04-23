01d74ccb096ccdfded9865290f89fc5b69021ad8
Jeff <jeff@Jeffs-MacBook-Pro.local>
2019-04-23 14:37:42 -0400

    make changes in vi

--- NetBeans3/src/App.java
+++ NetBeans3/src/App.java
@@ -15,6 +15,7 @@ public class App {
      */
     public static void main(String[] args) {
         System.out.println("Hello World!");
+	System.out.println("I'm editing code in vi. It's pretty bad.");
     }
     
 }
